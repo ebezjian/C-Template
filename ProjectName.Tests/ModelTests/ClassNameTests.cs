fc36d6d994176160f8ff2e230f82a4dda040e40b
Ellie Lambert <ebezjian@gmail.com>
2020-12-08 12:45:12 -0500

    Initial Commit

---
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName; //TODO - Change name
+
+namespace ProjectName.Tests //TODO - Change name
+{
+  [TestClass]
+  public class ClassNameTests //TODO - Change name
+  {
+//     [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+// }
+
+  }
+}
---
+++ ProjectName/Models/ClassName.cs
@@ -0,0 +1,7 @@
+namespace ProjectName //TODO - Change name Business Logic
+{
+  public class ClassName //TODO - change name
+  {
+    // properties, constructors, methods, etc. go here
+  }
+}
